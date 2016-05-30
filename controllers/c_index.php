7e6aabb5a88f348fb05fa32d0cd513af03b8cd1a
Rusu Ovidiu-Mihai <ovidiu-mihai.rusu@my.fmi.unibuc.ro>
2016-05-30 15:18:22 +0300

    small modif

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -9,6 +9,7 @@ class C_index extends Controller{
 	public function index()
 	{	
 		$vars['rows'] = $this->m_recipes->select_all('r.*,CONCAT(u.name," ",u.surname) as name_u, u.username as username','1','r left join useri u on r.id_user=u.id');
+		$vars['useri'] = $this->m_useri->selectCol('*','1 ORDER BY score DESC');
 		$this->view->title = 'TheCookingPot';
 		$this->view->render('index/index', $vars);
 	}
