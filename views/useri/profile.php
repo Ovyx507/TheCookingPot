67d5b4d02d2c1bc76a3054ad761a534a5476752b
cioroianudenis <cioroianu.denis@gmail.com>
2016-05-30 11:16:29 +0300

    update

--- views/useri/profile.php
+++ views/useri/profile.php
@@ -2,6 +2,7 @@
 .list-group-item {
 display:list-item;
 }
+.classifier{min-width:100px;font-family:Arial;font-weight:600;display:inline-block;}
 </style>
 </br>
 <div class="row">
@@ -12,5 +13,9 @@ display:list-item;
 <? echo $vars['meniu'];?>
 <hr>
 <div id="profile">
- 
+	<span class="classifier">E-mail:</span><span><? echo $vars['row']['email']; ?></span>
+	<br/>
+	<span class="classifier">Nume:</span><span><? echo $vars['row']['name']; ?></span>
+	<br/>
+	<span class="classifier">Prenume:</span><span><? echo $vars['row']['surname']; ?></span>
 </div>
